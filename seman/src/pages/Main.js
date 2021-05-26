36cfd0d62867d216ae27caea28782485912b0957
hyeonggeun2 <dhgudrms12@gmail.com>
2021-05-26 14:30:13 +0900

    Main 페이지 Carousel 추가

--- seman/src/pages/Main.js
+++ seman/src/pages/Main.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Main.css"
 import Nav from '../components/Nav/Nav';
-import Footer from '../components/Footer/Footer'
+import Footer from '../components/Footer/Footer';
+import MyCarousel from '../components/Carousel/Carousel';
 
 function Main() {
   return (
     <div>
       <Nav />
       <div className="main">
-        MAIN
+      <MyCarousel/>
       </div>
       <Footer />
     </div>
