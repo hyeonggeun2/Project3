4d1746f113b4bef1f2ac317493ff20d437e799b5
hyeonggeun2 <dhgudrms12@gmail.com>
2021-05-27 14:43:13 +0900

    router 추가

--- seman/src/pages/index.js
+++ seman/src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Main } from './Main';
 export { default as Category } from './Category';
 export { default as Detail } from './Detail';
+export { default as Login } from './Login';
+export { default as SignUp } from './SignUp';
